7ac84829076ad0c06c3bc937ef5030bbac5f6312
Uldis Sturms <uldis.sturms@gmail.com>
2015-10-25 20:08:45 +0000

    Allow nodejs hello world app to run on any port

--- src/nodejs/app.js
+++ src/nodejs/app.js
@@ -2,4 +2,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200);
   res.end('Hello World');
-}).listen(3000, '127.0.0.1');
+}).listen(process.env.PORT || 3000, '127.0.0.1');
